a0a8c9b0c9d69f5258856c88bf2efb90dcfdaab5
nate-moo <40650681+nate-moo@users.noreply.github.com>
2021-12-21 14:00:54 -0500

    Epic Additions (Adding everything that isn't templated)

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../tailwind.css';
+	import '../app.css';
+</script>
+
+<nav class="flex bg-gray-900">
+    <div class="flex-auto m-4 text-lg text-white">
+        <a href="/Egg">Nate's Photography Ventures</a>
+        <a href="/" class="p-2">Home</a>
+        <a href="/About" class="p-2">About</a>
+    </div>
+</nav>
+
+<slot />
---
+++ src/routes/bio.svelte
@@ -0,0 +1,6 @@
+<script>
+
+</script>
+<main>
+    <p>This is the BIO</p>
+</main>
